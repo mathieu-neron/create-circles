1b5375bed865feeaac6fb9ce93af966a23dd89ad
mneron <mathieu.neron@gmail.com>
2023-04-09 00:10:34 -0700

    implementation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { type } from 'os';
+
+interface Coords {
+  x: number;
+  y: number;
+}
+
+function Circle(props: Coords) {
+  return (
+    <div className="Circle" style={{left: `${props.x}px`, top: `${props.y}px`,}}>
+    </div>
+  );
+}
 
 function App() {
+  const [circles, setCircles] = useState<Coords[]>([]);
+  const [undoStack, setUndoStack] = useState<Coords[]>([]);
+  const divElement = useRef(null);
+
+  const handleClick: React.MouseEventHandler<HTMLDivElement> = (e) => {
+    const x = e.pageX;
+    const y = e.pageY;
+    setCircles([...circles, {x, y}]);
+  };
+
+  const handleUndo = () => {
+    if (circles.length === 0) return;
+    const last = circles.pop();
+    setCircles([...circles]);
+    if (last) setUndoStack([...undoStack, last]);
+  };
+
+  const handleRedo = () => {
+    if (undoStack.length === 0) return;
+    const last = undoStack.pop();
+    setUndoStack([...undoStack]);
+    if (last) setCircles([...circles, last]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <button disabled={circles.length === 0} onClick={handleUndo}>Undo</button>
+    <button disabled={undoStack.length === 0} onClick={handleRedo}>Redo</button>
+    <div ref={divElement} className="App" onClick={handleClick}>
+      {circles.map((circle, idx) => (
+        <Circle key={idx} x={circle.x} y={circle.y} />
+      ))}
     </div>
+    </>
   );
 }
 
